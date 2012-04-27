515a1c21a2470c11cd9512f4a67d1f10f14d1a28
wakkunn <wataru@oiax.jp>
2012-04-27 13:43:10 +0900

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Planets::Application.config.secret_token = 'bb43e08b672754e43322eacfb723db46ec50ff1672c2af478252e59af1d828bb4def06ac3a10b540c16d6a8b270b69c7c66e7c9d4a2bba45f3c79f3c4e82d1d4'
